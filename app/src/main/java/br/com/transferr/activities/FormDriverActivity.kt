cb0b8fd82d510c4a6a2b360b5b0e29fb637f2f6a
rafasystec <rafasystec@yahoo.com.br>
2018-02-09 20:20:29 -0300

    implementing background location service

---
+++ app/src/main/java/br/com/transferr/activities/FormDriverActivity.kt
@@ -0,0 +1,13 @@
+package br.com.transferr.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import br.com.transferr.R
+
+class FormDriverActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_driver)
+    }
+}
