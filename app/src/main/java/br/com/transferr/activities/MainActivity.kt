aefe885ce0e9078b13a1e14063d08537ae01ac80
Rafael Rocha <rafasystec@yahoo.com.br>
2018-02-10 00:41:05 -0300

    Data form for driver personal information.

--- app/src/main/java/br/com/transferr/activities/MainActivity.kt
+++ app/src/main/java/br/com/transferr/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import br.com.transferr.R
 import br.com.transferr.extensions.setupToolbar
+import br.com.transferr.services.LocationTrackingService
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
@@ -12,21 +13,38 @@ class MainActivity : SuperClassActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupToolbar(R.id.toolbar)
+        //setupToolbar(R.id.toolbar)
         //btnLocation.setOnClickListener{callInitialActivity()}
+        btnFrmDriver.setOnClickListener { callFormDriver() }
         swtOnline.setOnClickListener { stopInitLocation() }
+        stopInitLocation()
     }
-
+/*
     fun callInitialActivity(){
         val intentInit = Intent(context,InitialActivity::class.java)
+        startActivity(intentInit)
+    }
+*/
+    fun callFormDriver(){
+        val intentInit = Intent(context,FormDriverActivity::class.java)
         startActivity(intentInit)
     }
 
     private fun stopInitLocation(){
         if(swtOnline.isChecked){
             toast("Vc agoa esta online")
+            startService()
         }else{
-            toast("Vc esta off")
+            toast("Vc esta off 7")
+            stopService()
         }
+    }
+
+    private fun startService(){
+        startService(Intent(this, LocationTrackingService::class.java))
+    }
+
+    private fun stopService(){
+        stopService(Intent(this,LocationTrackingService::class.java))
     }
 }
--- app/src/main/java/br/com/transferr/services/LocationTrackingService.kt
+++ app/src/main/java/br/com/transferr/services/LocationTrackingService.kt
@@ -4,9 +4,17 @@ import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.location.LocationManager
 import android.util.Log
+import br.com.transferr.model.Car
+import br.com.transferr.model.Coordinates
+import br.com.transferr.model.Driver
+import br.com.transferr.model.enums.EnumStatus
+import br.com.transferr.webservices.CoordinateService
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 
 class LocationTrackingService : Service() {
 
@@ -16,6 +24,8 @@ class LocationTrackingService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
+        toast("starting service")
+        runs=true
         return START_STICKY
     }
 
@@ -24,7 +34,8 @@ class LocationTrackingService : Service() {
     override fun onCreate() {
         if (locationManager == null)
             locationManager = applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
+        Log.d(TAG,"on create")
+        runs = true
         doAsync { run() }
 
 
@@ -32,18 +43,52 @@ class LocationTrackingService : Service() {
 
     override fun onDestroy() {
         super.onDestroy()
+        toast("destroying the service 1")
+        runs=false
+    }
+
+    companion object {
+
+        var runs = true
     }
 
     @SuppressLint("MissingPermission")
     private fun run(){
         var location = locationManager?.getLastKnownLocation(LocationManager.GPS_PROVIDER)
         Log.d(TAG,"Location in  onCreated ${location?.latitude} - ${location?.longitude}")
-        while (true){
+        while (runs){
             Thread.sleep(2*1000)
             location = locationManager?.getLastKnownLocation(LocationManager.GPS_PROVIDER)
             Log.d(TAG,"Location in  onCreated ${location?.latitude} - ${location?.longitude}")
+            //callWebService(location)
         }
 
+    }
+
+    private fun callWebService(location: Location?) {
+        var car: Car = getDefaultCar()
+        var coordinates = Coordinates()
+        coordinates.id = 0
+        coordinates.car         = car
+        coordinates.latitude    = location?.latitude
+        coordinates.longitude   = location?.longitude
+        doAsync {
+            val response = CoordinateService.save(coordinates)
+            uiThread {
+                var msg = response.msg
+                if(msg == null){
+                    msg = "Invalid Message"
+                }
+                toast(msg)
+                //finish()
+            }
+        }
+    }
+
+    private fun getDefaultCar(): Car {
+        var driver: Driver = Driver("Jose","664764", 19880305)
+        var car:Car = Car("cheve","2215563","azul",true,driver, EnumStatus.OFFLINE)
+        return car
     }
 
 }
