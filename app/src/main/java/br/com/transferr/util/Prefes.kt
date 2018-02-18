f4f10f38371d063fed2d60099c69614b5471fee3
Rafael Rocha <rafasystec@yahoo.com.br>
2018-02-18 10:51:02 -0300

    Starting to implement the take picture function

--- app/src/main/java/br/com/transferr/util/Prefes.kt
+++ app/src/main/java/br/com/transferr/util/Prefes.kt
@@ -3,16 +3,27 @@ package br.com.transferr.util
 import android.content.Context
 import android.content.SharedPreferences
 import android.graphics.Color
+import br.com.transferr.application.ApplicationTransferr
 
 /**
  * Created by root on 12/02/18.
  */
-class Prefes (context: Context) {
+object Prefes {
     val PREFS_FILENAME      = "br.com.transferr.prefs"
     val ID_DRIVER           = "br.com.transferr.driver.id"
-    val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
+    //val DRIVER              = "br.com.transferr.driver"
+    //val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0)
 
+    private fun prefs() : SharedPreferences{
+        val contex = ApplicationTransferr.getInstance().applicationContext
+        return contex.getSharedPreferences(PREFS_FILENAME,0)
+    }
     var prefsLogin: Int
-        get() = prefs.getInt(ID_DRIVER,0)
-        set(value) = prefs.edit().putInt(ID_DRIVER, value).apply()
+        get() = prefs().getInt(ID_DRIVER,0)
+        set(value) = prefs().edit().putInt(ID_DRIVER, value).apply()
+    //Use to save other values
+    fun setInt(flag:String,value:Int) = prefs().edit().putInt(flag,value).apply()
+    fun getInt(flag: String)= prefs().getInt(flag,0)
+    fun setString(flag: String,value:String) = prefs().edit().putString(flag,value).apply()
+
 }
